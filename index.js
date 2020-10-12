16eb930b984ac4f1ef3955da67631de3c11f2c12
hongjuns <asws12345@naver.com>
2020-10-12 23:09:00 +0900

    mongodb 설정 완료

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const mongodbUser = require('./lib/mongodbUser');
 const app = express();
 const port = 3000
+
+
+mongoose.connect(mongodbUser.mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => console.log('MongoDB connected ...'))
+  .catch(err => console.log(err))
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
