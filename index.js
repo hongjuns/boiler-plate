92069e5885ef7cbb2ec9ecf5d858561890e36834
hongjuns <asws12345@naver.com>
2020-10-14 23:51:40 +0900

    register api 생성

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const mongodbUser = require('./lib/mongodbUser');
 const app = express();
 const port = 3000
 
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 mongoose.connect(mongodbUser.mongodbUrl, {
   useNewUrlParser: true,
@@ -12,6 +17,25 @@ mongoose.connect(mongodbUser.mongodbUrl, {
   useCreateIndex: true
 }).then(() => console.log('MongoDB connected ...'))
   .catch(err => console.log(err))
+
+
+app.post('/register', (req,res)=> {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+
+    if (err) return res.json({
+      success: false,
+      err
+    })
+    
+    return res.status(200).json({
+      success : true,
+      msg :'회원가입이 완료 되었습니다.'
+    })
+  })
+
+})
 
 
 app.get('/', function (req, res) {
