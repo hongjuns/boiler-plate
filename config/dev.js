ebdef70af82b78c41e0fc115c7d4c8ea40f072be
hongjuns <asws12345@naver.com>
2020-10-18 21:56:59 +0900

    config파일 업로드

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+const MONGO_URI ='mongodb+srv://hongjun:jls16440500@boilerplate.vg1od.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI : process.env.MONGO_URI
+}
+
